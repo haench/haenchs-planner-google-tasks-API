fa30e676dfb99016e0cdbc30cb0df30043074647
haench <haench@gmx.de>
2019-03-30 11:06:51 +0100

    Updates

--- src/components/editButton.js
+++
@@ -1,8 +0,0 @@
-import styled from "styled-components";
-import { Edit as EditIcon } from "styled-icons/feather/Edit";
-
-const EditButton = styled(EditIcon)`
-  color: ${props => props.color || "#eee"};
-  display: ${props => (props.hidden ? "none" : "inherent")};
-`;
-export default EditButton;
--- src/config.js
+++
@@ -1,4 +0,0 @@
-export default {
-  clientId: "922974798829" // development
-  // clientId: "W89NAKXBQXDINyLSem_nTXVj" // haenchs planner netlify
-};
