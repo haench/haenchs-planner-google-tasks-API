f8970c0debafddfb602a3bd0913eecbc47c4d906
haench <haench@gmx.de>
2019-04-13 20:22:54 +0200

    Update

--- src/components/calendar/calendar.js
+++ src/components/calendar/calendar.js
@@ -10,16 +10,16 @@ import "@fullcalendar/daygrid/main.css";
 import "components/calendar/fc_finetuning.css";
 
 const Calendar = props => (
-  <CalendarPane>
-    <FullCalendar
-      defaultView={"dayGridMonth"}
-      plugins={[dayGridPlugin]}
-      events={eventStore.events}
-      weekNumberCalculation={"iso"}
-      firstDay={1}
-      showNonCurrentDates={false}
-    />
-  </CalendarPane>
+  // <CalendarPane>
+  <FullCalendar
+    defaultView={"dayGridMonth"}
+    plugins={[dayGridPlugin]}
+    events={eventStore.events}
+    weekNumberCalculation={"iso"}
+    firstDay={1}
+    showNonCurrentDates={false}
+  />
+  // </CalendarPane>
 );
 
 export default view(Calendar);
